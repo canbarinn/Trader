5e0e5c5036610aef8641ea090d12a7962ec97ba5
Egemen Göl <egemengol@gmail.com>
2023-07-08 17:17:05 +0300

    comments

--- contracts/MockSwapRouter.sol
+++ contracts/MockSwapRouter.sol
@@ -8,46 +8,21 @@ import "../interfaces/IMockSwapRouter.sol";
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
-// TASK: Research sqrtPriceLimit and amountOutMinimum
-// TASK: Now, we are only swapping tokens, implement amountoutMin and sqrtPriceLimit after research
-
 contract MockSwapRouter is IMockSwapRouter {
     using SafeERC20 for IERC20;
 
-    address public tokenA; // change address type to MockToken IERC20 type
+    address public tokenA;
     address public tokenB;
-
-    // address private owner;
-    // .... public price; // can change with a function call
-    // price will be used as 1 tokenA = price x tokenB
-    // uint256 public price;
 
     constructor(address _tokenA, address _tokenB) {
         tokenA = _tokenA;
         tokenB = _tokenB;
-        // owner = msg.sender;
-        // set price
     }
 
-    // modifier onlyOwner() {
-    //     require(msg.sender == owner, "You are not the owner!");
-    //     _;
-    // }
-
-    // can withdraw tokens from contract
-    // CANQ: swap islemi yaparken tokenlari direkt olarak msg.senderlara yolladigimiz icin
-    // withdraw gerekli olmayabilir
-
-    // can change price
-    // function setPrice(uint newPrice) external onlyOwner {
-    //     price = newPrice;
-    // }
-
-    //swap
     function swapExactInputSingle(
         ISwapRouter.ExactInputSingleParams calldata params
     ) external {
-        // transfer A in
+        // revert if params.tokenIn != tokenA || params.tokenOut != tokenB
         IERC20(tokenA).safeTransferFrom(
             msg.sender,
             address(this),
@@ -55,18 +30,7 @@ contract MockSwapRouter is IMockSwapRouter {
         );
 
         IERC20(tokenB).safeTransfer(msg.sender, params.amountIn);
-
-        // TASK: if needed to do more like unsiwap
-        // function exactInputSingle(params)internal; this function will do the necessary calculations for MockSwapRouter
-        //it will check min and max amounts, sqrtpricelimit and also it interacts with pools for price calculations,
-        //use it later, with params used as struct at the beginning of the contract
     }
 
-    //swap
-    // function swapExactOutputSingle(uint amountOut) external {
-    //     IERC20(tokenA).safeApprove(address(this), amountIn);
-    //     IERC20(tokenA).safeTransferFrom(msg.sender, address(this), amountIn);
-
-    //     IERC20(tokenB).safeTransfer(msg.sender, amountIn);
-    // }
+    // function swapExactOutputSingle
 }
--- interfaces/ILimitTrader.sol
+++ interfaces/ILimitTrader.sol
@@ -1,13 +1,5 @@
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 interface ILimitTrader {
-    // LimitOrder: {For this pair, I want to swap at this price, this much amount} (struct)
-    // set at most one limit order
-    // remove limit order
-    // get limit order status
-    //
-    // can limit order be filled at the moment
-    //      Look at the price, if it is better than the limit price, then return true
-    // fill the limit order
-    //      try to fill the limit order without checking, if not, reverts
+    function triggerSwap(uint256 spendAmount) external returns (uint256);
 }
